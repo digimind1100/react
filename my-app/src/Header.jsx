849772a6c6a78b8099a394a4d73433db1df96fa1
urooj haider <suhz1100@gmail.com>
2025-09-24 18:24:42 +0500

    add

---
+++ my-app/src/Header.jsx
@@ -0,0 +1,25 @@
+
+
+
+function Header (){
+
+    return(
+<>
+<header>
+    <div className="flex justify-center">
+        <img src="./assets.logo" ></img>
+<ul className="flex justify-center">
+    
+    <li className="m-1.5 p-1.5 text-xl " ><a href="#">Home</a></li>
+    <li className="m-1.5 p-1.5 text-xl "><a href="#">About</a></li>
+    <li className="m-1.5 p-1.5 text-xl "><a href="#">Product</a></li>
+    <li className="m-1.5 p-1.5 text-xl "><a href="#">Portfolio</a></li>
+    <li className="m-1.5 p-1.5 text-xl "><a href="#">Contact</a></li>
+</ul>
+</div>
+</header>
+</>
+)
+}
+
+export default Header
